8295d348089a7344854dabb13aeb3e963f3393c9
hassanwebdeveloper <h.baig34@gmail.com>
2022-05-11 23:57:24 +0500

    pixel wise average of multiple models output in ensemble

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ from options.test_options import TestOptions
 from data import create_dataset
 from models import create_model
 from util.visualizer import save_images
-from util import html
+from util import html, tensor2im
 
 
 if __name__ == '__main__':
@@ -39,8 +39,7 @@ if __name__ == '__main__':
     opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
     opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-    model = create_model(opt)      # create a model given opt.model and other options
-    model.setup(opt)               # regular setup: load and print networks; create schedulers
+    
     # create a website
     web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
     if opt.load_iter > 0:  # load_iter is 0 by default
@@ -49,16 +48,40 @@ if __name__ == '__main__':
     webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
     # test with eval mode. This only affects layers like batchnorm and dropout.
     # For [pix2pix]: we use batchnorm and dropout in the original pix2pix. You can experiment it with and without eval() mode.
-    # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
-    if opt.eval:
-        model.eval()
+    # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.            
+    lst_models = []
+    
+    n_models = opt.n_models
     for i, data in enumerate(dataset):
-        if i >= opt.num_test:  # only apply our model to opt.num_test images.
-            break
-        model.set_input(data)  # unpack data from data loader
-        model.test()           # run inference
-        visuals = model.get_current_visuals()  # get image results
-        img_path = model.get_image_paths()     # get image paths
+        lst_imobj = {}
+        for j in range(1, n_models + 1, 1):
+            opt.modelnumber = j
+            model = create_model(opt)      # create a model given opt.model and other options
+            model.setup(opt)               # regular setup: load and print networks; create schedulers
+            if opt.eval:
+                model.eval()
+            if i >= opt.num_test:  # only apply our model to opt.num_test images.
+                break
+            model.set_input(data)  # unpack data from data loader
+            model.test()           # run inference
+            visuals = model.get_current_visuals()  # get image results
+            
+            for label, im_data in visuals.items():
+                if label in lst_imobj:
+                    arr_imgs = lst_imobj[label]
+                    arr_imgs.append(im_data)                    
+                else:
+                    arr_imgs = []
+                    arr_imgs.append(im_data)
+                
+                lst_imobj[label] = arr_imgs
+        
+        for lable in lst_imobj:
+            arr_imdata = lst_imobj[label]
+            mean = torch.mean(torch.stack(arr_imdata))
+            visuals[label] = mean
+            
+        img_path = model.get_image_paths()     # get image paths        
         if i % 2 == 0:  # save images to an HTML file
             print('processing (%04d)-th image... %s' % (i, img_path))
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
